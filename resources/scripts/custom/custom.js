561a25c1fc879ceb884012e2b055cbfccafc5bf4
Murali Krishna <vmurali1444@gmail.com>
2016-09-15 12:53:26 +0530

    Partially done with hash map

--- resources/scripts/custom/custom.js
+++ resources/scripts/custom/custom.js
@@ -1,1 +1,35 @@
-console.log($);
+$(document).ready(function(){
+
+  var configuredWebUrls = {};
+  var WebUrlsListDOM = $("#webSourcesList");
+
+  $(".add-url").click(function(e){
+    var url = $($(this).parent().children()[1])[0].innerText.trim();
+    if(!configuredWebUrls.hasOwnProperty(url)) {
+      configuredWebUrls[url] = $(this)[0].id;
+      updateConfiguredWebUrlsDOM();
+    }
+    console.log(configuredWebUrls)
+  });
+
+  function removeUrl(e, url){
+    console.log("removeing url==>", url, configuredWebUrls)
+    delete configuredWebUrls[url];
+    updateConfiguredWebUrlsDOM();
+  }
+
+  WebUrlsListDOM.click(function(e) {
+    console.log($(e.target).parents("li"));
+  })
+
+  function updateConfiguredWebUrlsDOM() {
+    WebUrlsListDOM.empty();
+    for(var url in configuredWebUrls){
+      var li = $("<li>");
+      li.append(url);
+      li.append($("<span class='remove-selected-url'><i class='fa fa-close'></i></span>"));
+      // li.find(".remove-selected-url").click(function(e){removeUrl(e, url)});
+      WebUrlsListDOM.append(li);
+    }
+  }
+});
