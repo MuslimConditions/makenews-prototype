fcc6e6d2230f37dfab6abd830420818c2fde199f
Vankayalapati Nagadurga <nvankaya@thoughtworks.com>
2016-09-22 09:51:00 +0530

    [Nag/Sai] changed the UI of bookmark page

--- resources/scripts/custom/getBookmarks.js
+++ resources/scripts/custom/getBookmarks.js
@@ -1,23 +1,30 @@
 $(document).ready(function () {
 
-        var cList = $('ul.myBookmarks');
+        var cList = $('#bookmark_articles');
 
         var bookmarkList = JSON.parse(localStorage.getItem("bookmark_data"));
-        for (var index = 0; index < bookmarkList.length; index++) {
-            var li = "<li>";
-            li+=("<b>"+bookmarkList[index].title+"</b>"+"</br>"+"</br>");
-            li+=(bookmarkList[index].body.slice(0,100)+"</br>"+"</br>");
-            li+=(bookmarkList[index].source+"</br>");
-            li+="</li>";
-            li+="<hr>";
-            cList.append(li);
+        var body=bookmarkList[0].body.slice(0,100);
 
+        for (var index = 0; index < bookmarkList.length; index++) {
+            var title = bookmarkList[index].title;
+            var source = bookmarkList[index].source;
+
+            var li =$("<li class='bookmark__item'><p class='bookmark__item__title'>"+title
+                    +"</p><p class='bookmark__item__summary'>"+body
+                    +"</p><div class='bookmark__item__source-date'><span id='bookmark__item__source'>"
+                    +source+"</span> | Aug 21, 2016, 08.23 PM IST</div></li>");
+            cList.append(li);
 
         }
 
-    $(".article__title").replaceWith(JSON.parse(localStorage.getItem("bookmark_data"))[0].title);
-    $(".article__source").replaceWith(JSON.parse(localStorage.getItem("bookmark_data"))[0].source);
+         $(".bookmark__item").click(function(event){showSelectedArticle(event)});
+
+         function showSelectedArticle(event) {
+            var $currentItem = $(event.currentTarget);
+            $(".article__title").text($currentItem.find(".bookmark__item__title").text());
+            $(".article__source").text($currentItem.find("#bookmark__item__source").text());
+         }
+
     $(".article__body").replaceWith(JSON.parse(localStorage.getItem("bookmark_data"))[0].body);
 
-    }
-);
+    });
