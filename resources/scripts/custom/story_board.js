c57ae692a9ec1b8e51aaeea1a11a5f4af8897d13
Mahitha Arveti <marveti@thoughtworks.com>
2016-09-29 17:26:43 +0530

    [Mahitha/Nag]- story board- saved stories to local storage and renders from storage

--- resources/scripts/custom/story_board.js
+++ resources/scripts/custom/story_board.js
@@ -1,4 +1,17 @@
 $(document).ready(function(){
+    var storiesList=[];
+    var savedStories=$("#savedStories");
+    storiesList=JSON.parse(localStorage.getItem("Stories"))||[];
+
+    renderStories();
+
+    function renderStories(){
+        for(var i=0; i<storiesList.length; i++){
+            var li="<li class='story'> <span class='story-title'>"+storiesList[i].title+"</span> <span class='story-content'>"+storiesList[i].content+"</span> </li>";
+            renderStory(storiesList[i].title, storiesList[i].content);
+        }
+    }
+
     $("#u5078").click(function(){
         var title = document.getElementById("u5073_input").value;
         document.getElementsByClassName("story-box")[0].innerHTML = title;
@@ -11,14 +24,23 @@ $(document).ready(function(){
        document.getElementsByClassName("story-board")[0].style.display = 'none';
        document.getElementsByClassName("add-story")[0].style.display = 'block';
         document.getElementsByClassName('done-img')[0].style.pointerEvents = 'auto';
+        document.getElementsByClassName("story-title")[0].value ="";
+        document.getElementsByClassName("story-content")[0].value="";
 
     });
     $(document).on('click', '.story-box',function(){
+        var title = this.innerText;
+         $(".back_to_story_board_header").css({'display':'block'});
        document.getElementsByClassName("story-board")[0].style.display = 'none';
        document.getElementsByClassName("add-story")[0].style.display = 'block';
        document.getElementsByClassName('done-img')[0].style.pointerEvents = 'auto';
-        document.getElementsByClassName("story-title")[0].value=this.innerText;
-        document.getElementsByClassName("story-content")[0].value=localStorage.getItem(this.innerText);
+        document.getElementsByClassName("story-title")[0].value=title;
+        storiesList.forEach(function(story){
+            if(story.title === title){
+                document.getElementsByClassName("story-content")[0].value=story.content;
+            }
+        });
+
 
     });
 
@@ -29,10 +51,14 @@ $(document).ready(function(){
     });
 
     $(".done-img").click(function(){
-        if(localStorage.getItem(document.getElementsByClassName("story-title")[0].value) == null){
+
+
+        console.log(($(document.getElementsByClassName("add-story")).children())[0].value);
+        console.log(($(document.getElementsByClassName("add-story")).children())[1].value);
+
             var newBox = document.createElement('div');
             newBox.className = "story-box";
-            newBox.innerHTML = document.getElementsByClassName("story-title")[0].value;
+            newBox.innerHTML = ($(document.getElementsByClassName("add-story")).children())[0].value
             var imageTag  = document.createElement('img');
             imageTag.src= 'images/news_board__collections/delete_u2816.png';
             imageTag.className = 'story-image-tag';
@@ -49,16 +75,20 @@ $(document).ready(function(){
                 }
                 $(newBox).insertAfter(document.getElementsByClassName('story-box')[elements.length - 1]);
             }
-        }
-        localStorage.setItem(document.getElementsByClassName("story-title")[0].value, document.getElementsByClassName("story-content")[0].value);
+
+//        localStorage.setItem(document.getElementsByClassName("story-title")[0].value, document.getElementsByClassName("story-content")[0].value);
+
+        storiesList.push({'title' :($(document.getElementsByClassName("add-story")).children())[0].value, 'content' : ($(document.getElementsByClassName("add-story")).children())[1].value });
+        localStorage.setItem("Stories",JSON.stringify(storiesList));
+
         document.getElementsByClassName("story-board")[0].style.display = 'block';
         document.getElementsByClassName("add-story")[0].style.display = 'none';
-                $(".back_to_story_board_header").css({'display':'none'});
+         $(".back_to_story_board_header").css({'display':'none'});
         document.getElementsByClassName('done-img')[0].style.pointerEvents = 'none';
-        document.getElementsByClassName("story-title")[0].value ="Story Title";
-        document.getElementsByClassName("story-content")[0].value="";
+        document.getElementsByClassName("story-title").value ="";
+        document.getElementsByClassName("story-content").value="";
 
-
+        window.location.reload();
     });
 
     $("#u5880").click(function() {
@@ -70,4 +100,27 @@ $(document).ready(function(){
         $(".story-board").css({'display':'block'});
         $(".back_to_story_board_header").css({'display':'none'});
     });
+
+    function renderStory(title,content){
+        var newBox = document.createElement('div');
+        newBox.className = "story-box";
+        newBox.innerHTML = title;
+        var imageTag  = document.createElement('img');
+        imageTag.src= 'images/news_board__collections/delete_u2816.png';
+        imageTag.className = 'story-image-tag';
+        newBox.appendChild(imageTag);
+        var elements = document.getElementsByClassName('story-box');
+        if (elements.length === 0) {
+            newBox.style.marginTop='67px';
+            newBox.style.marginLeft='256px';
+            $(newBox).insertAfter(document.getElementsByClassName('story-box-1')[0]);
+        }
+        else {
+            if (elements.length === 1) {
+                newBox.style.marginTop = '67px';
+            }
+            $(newBox).insertAfter(document.getElementsByClassName('story-box')[elements.length - 1]);
+        }
+
+    }
 });
